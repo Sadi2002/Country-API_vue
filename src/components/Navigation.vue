135c66d1770b1123cdcad9ccf0860ae020507475
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2024-01-03 10:55:12 +0100

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navigation">
+    <h2 class="navigation-title">Where is the world?</h2>
+    <div class="navigation-theme-mode">
+      <i class="far fa-moon"></i>
+      <span>Dark Mode</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
